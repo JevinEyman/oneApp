ef43dceae73458d92ca625c6facf6bab86e96745
Armand Guim <armandguim3@gmail.com>
2018-04-16 05:25:42 -0400

    brand not rendering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,11 @@ import "../styles/Navbar.css";
 const Navbar = () => (
   <div>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">OneApp</a>
+      <a className="navbar-brand" href="#"><img src="./src/logo2.png"/>></a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
+        <span className="navbar-toggler-icon">
+        
+        </span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
         <ul className="navbar-nav">

40f6ed83abac51b34040ff4fcc9b286080c4d0f9
Jevin Eyman <jevin.eyman@github.io>
2018-04-16 22:02:14 -0400

    youtube div

--- src/components/Youtube.js
+++ src/components/Youtube.js
@@ -1,28 +1,31 @@
 import React, {Component} from 'react';
 
-const API = "AIzaSyBHeRj0l7ALC_Y9wixnnuHpZEVKYD70vVU"
-const channelID = "69fCpIY2hO8&t=1262s"
-const results = 10;
+const API = "AIzaSyAOYG1Ai4mZy6L-ifZgQ8bzS87vA6v3JdA"
+const channelID = "69fCpIY2hO8&t=2s"
+const result = 10;
+const baseURL="https://www.googleapis.com/youtube/v3/search?key="  
+//const videoID= "69fCpIY2hO8&list=RD69fCpIY2hO8&t=48"
 
-var finalURL = `${API}&channelID=${channelID}&part=snippet,id&order=date&maxResults=${results}`
+var finalURL = `${baseURL}${API}&channelID=${channelID}&part=snippet,id&order=date&maxResults=${result}`
 
 
 class Youtube extends Component {
 
-  constructor (props){
+  constructor(props){
     super(props);
     this.state = {
-      resultsyt: []
+      resultyt: []
     };
   this.clicked= this.clicked.bind(this);
   }
     
     clicked(){
-      fetch (finalURL)
-      .then ((response) => response.json())
-      .then (( responseJson) => {
-        const resultsyt = responseJson.items.map(obj => "https://www.youtube.com/embed/"+obj.id.videoID);
-        this.setState({resultsyt});
+     fetch(finalURL)
+      .then((response) => response.json())
+      .then(( responseJson) => {
+        const resultyt = responseJson.items.map(obj => "https://www.youtube.com/embed/"+obj.id.videoId);
+        this.setState({resultyt: resultyt});
+        //console.log(responseJson);
       })
       .catch((error)=>{
         console.error(error);
@@ -30,35 +33,47 @@ class Youtube extends Component {
     }
   
     render(){
-      console.log(this.state.resultsyt);
+      //console.log(finalURL);
     
       
   return(
-      /*<div>
-          <button onClick = {this.clicked}>Get Productivity Music Video</button>
-    
-     this.state.resultsyt.map((link, i)={
+      <div>
+          <button onClick={this.clicked}>Get Productivity Music Video</button>
+    {
+      this.state.resultyt.map((link, i) => {
 
-      var frame= <div key={i} className="youtube"><iframe  width="560" height="315" src={link} frameBorder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
+       var frame = <div key={i} className="youtube"><iframe  width="560" height="315" src={link} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe></div>
                 
-      }      
+            
       return frame;
               
-              )
+      })
+
+    }
               
           {this.frame}
       
-      </div>*/
-    
-       <div>
-         <button onClick = {this.clicked}>Get Productivity Music Video</button>
-       
-       <div> <iframe width="560" height="315" src="https://www.youtube.com/embed/69fCpIY2hO8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> </div>     
-     
       </div>
-     ); 
-    }
-  } 
+    
+    /*<div>
+         <button onClick = {this.clicked}>Get Productivity Music Video</button>
+
+        
+         
+       
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/69fCpIY2hO8" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>      
+         
+    </div>*/
+
+            );
+
+          }
+        }
+      
+      
+     
+
+   
     
   
 
